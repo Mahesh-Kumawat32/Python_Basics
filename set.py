83fe84c82de31e67029002c90450d5d938d037ed
Mahesh-Kumawat32 <mdkumawat3011@gmail.com>
2026-07-11 15:00:38 +0530

    add files

--- set.py
+++ set.py
@@ -1,17 +1,28 @@
 s = {1,2,3,4,54,5,23,12}
-
+k = {2,3,4,5,12}
 # for i in s:     #access element of set 
 #     print(i,end=" ")
 
-# s.add(12)                 #add()
-# s.update()                #update()
-# k = s.copy()              #copy()
+# s.add(12)                         #add()
+# s.update()                        #update()
+# k = s.copy()                      #copy()
 # print(k,end=" ")
-# s.clear()                 #clear()----->empty the set
-# s.discard(54)             #discard()    ----> remove a specific value if not exist don't so any error
-# s.remove(12)              #remove()----> remove a specific value from set
-# s.pop()                   #pop()----> remove a values randomly 
-# print(max(s))             #max()----->maximum value from a set
-# print(min(s))             #min()-----> minimum value from a set
-print(s.isdisjoint())
-                
+# s.clear()                         #clear()----->empty the set
+# s.discard(54)                     #discard()    ----> remove a specific value if not exist don't so any error
+# s.remove(12)                      #remove()----> remove a specific value from set
+# s.pop()                           #pop()----> remove a values randomly 
+# print(max(s))                     #max()----->maximum value from a set
+# print(min(s))                     #min()-----> minimum value from a set
+# print(s.isdisjoint(k))            #isdisjoint() ---->if both sets are not equal then gives true else false
+# print(k.issubset(s))              #issubset()
+# print(s.issuperset(k))            #issuperset()
+# print(s.union(k))                 #union()----give all value from both set but unique
+                                    #difference()--->
+# print(s.intersection(k))          #intersection() ---->give all value same in both sets 
+# print(s.symmetric_difference(k))  #symmetric_differencegive values from first set which are not in second set
+                                    #difference_update()--->
+                                    #intersection_update--->
+                                    #symmetric_difference_update()--->
+
+
+
